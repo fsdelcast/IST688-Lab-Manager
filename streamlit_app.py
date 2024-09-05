832668f69b2bfcd7f060161685e20173bfdb1c9e
Francisco del Castillo <156264874+fsdelcast@users.noreply.github.com>
2024-09-05 14:30:28 +0000

    Done lab 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from openai import OpenAI
 
-lab1_page = st.Page("Lab_1.py", title = 'Lab 1', icon="📄")
-lab2_page = st.Page("Lab_2.py", title = 'Lab 2', icon="📄")
+lab1_page = st.Page("Lab_1.py", title = 'Lab 1', icon="🥇")
+lab2_page = st.Page("Lab_2.py", title = 'Lab 2', icon="2️⃣")
 
 pg = st.navigation ([lab1_page,lab2_page])
 st.set_page_config(page_title = 'This is not showing',page_icon = '💯')
