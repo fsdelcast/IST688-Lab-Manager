feeb3ba034e7f28a427e92481153003c4dd16deb
Francisco del Castillo <156264874+fsdelcast@users.noreply.github.com>
2024-09-08 23:46:24 +0000

    Done

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from openai import OpenAI
 
-lab1_page = st.Page("Lab_1.py", title = 'Lab 1', icon="🥇")
-lab2_page = st.Page("Lab_2.py", title = 'Lab 2', icon="2️⃣")
+lab1_page = st.Page("./Labs/Lab_1.py", title = 'Lab 1', icon="🥇")
+lab2_page = st.Page("./Labs/Lab_2.py", title = 'Lab 2', icon="2️⃣")
 
 
 
